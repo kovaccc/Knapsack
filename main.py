a01ecda8c0da5a85aa6089f0c12eead35f639d0b
Matej Kovačević <kovacevic@uhp.digital>
2022-04-24 15:09:08 +0200

    counting memory usage

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import timeit
-
+import tracemalloc
 import dfs
 from item import Item
 import bfs
-# with deepcopy time is going over 0.5, much faster with shallow copy
 
 with open('Assignment 1 knapsack.txt') as knapsackFile:
     knapsack_file_lines = knapsackFile.readlines()
@@ -24,12 +23,26 @@ with open('Assignment 1 knapsack.txt') as knapsackFile:
         itemLine = [int(i) for i in line.split(" ")]
         items.append(Item(item_id=itemLine[0], weight=itemLine[2], benefit=itemLine[1]))
 
+    # BFS
+    tracemalloc.start()
+
     start = timeit.default_timer()
     bfs.breadth_first_search(max_weight=maxWeight, searched_items=items)
     stop = timeit.default_timer()
     print('BFS time: ', stop - start)
+
+    bfsSize, bfsPeak = tracemalloc.get_traced_memory()
+    print(f"Memory usage: {bfsSize * 0.000001}MB; Peak: {bfsPeak * 0.000001}MB")
+    tracemalloc.stop()
+
+    # DFS
+    tracemalloc.start()
+
     start = timeit.default_timer()
     dfs.depth_first_search(max_weight=maxWeight, searched_items=items)
     stop = timeit.default_timer()
     print('DFS time: ', stop - start)
 
+    dfsSize, dfsPeak = tracemalloc.get_traced_memory()
+    print(f"Memory usage: {dfsSize * 0.000001}MB; Peak: {dfsPeak * 0.000001}MB")
+    tracemalloc.stop()
